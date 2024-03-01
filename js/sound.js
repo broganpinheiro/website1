a484858ee34b94a48b9c23a006f3386bf77b5c3a
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-01 18:11:05 +0000

    Fri, Mar 1, 2024, 10:11 AM -08:00

--- js/sound.js
+++ js/sound.js
@@ -4,4 +4,18 @@ sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
 })
+
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}

042f188f1aa7e218af8cfca16703089180875eb6
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-06 17:06:28 +0000

    Wed, Mar 6, 2024, 9:06 AM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -6,4 +6,10 @@ window.addEventListener('scroll', fixnNav)
 
 function fixNav() {
     console.log(window.scrollY)
+    if (window.scrollY > 223) {
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
 }

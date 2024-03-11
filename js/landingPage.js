def470e54a44d08e39b766b354f52b0bf2c63da4
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-11 17:09:13 +0000

    Mon, Mar 11, 2024, 10:09 AM -07:00

---
+++ js/landingPage.js
@@ -0,0 +1,25 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+close = document.getElementById('close')
+
+// Toggle Nav
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+// Show Modal
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
+//Hide modal
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+})
+
+// Hide modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
+})

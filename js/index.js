08cd8c5a699a18abd90690789b1643e436bb051c
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-13 16:50:15 +0000

    Wed, Mar 13, 2024, 9:50 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 toggle = document.getElementById('toggle')
 container = document.querySelector('.container')
+button = document.querySelector('.fa-solid')
 
 toggle.addEventListener('click', () => {
     container.classList.toggle('inactive')
+    button.classList.toggle('fa-angle-down')
+    button.classList.toggle('fa-angle-up')
 })
+
 

b8bf1d7f8887f5df5aa046a5145e55340e2e8106
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-13 16:05:34 +0000

    Wed, Mar 13, 2024, 9:05 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 toggle = document.getElementById('toggle')
 
 toggle.addEventListener('click', () => {
+    div.classList.remove('active')
     div.classList.toggle('inactive')
+    div.classList.add('active')
 })
 
